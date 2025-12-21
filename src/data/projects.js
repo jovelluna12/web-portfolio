4d81c7bacf9205991b8e0df5a7e486cfd8a1469d
jovelluna12 <jchrister718@gmail.com>
2025-12-21 13:49:54 +0800

    chore: update projects list

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,22 @@
 const projects = [
   {
     id: 'p1',
+    title: 'Great White Sands Resorts | A Sample Resort Landing Page',
+    description: 'A Landing Page for a fictional resort.',
+    tech: ['JavaScript', 'Vue', 'Vite', 'CSS'],
+    link: 'https://sample-resort-portfolio.vercel.app/',
+    image: '',
+  },
+  {
+    id: 'p2',
+    title: 'Bistro Vue | A Sample Restaurant Landing Page',
+    description: 'A Landing Page for a fictional restaurant.',
+    tech: ['JavaScript', 'Vue', 'Vite', 'CSS'],
+    link: 'https://sample-restaurant-portfolio-yez2.vercel.app/',
+    image: '',
+  },
+  {
+    id: 'p3',
     title: 'Portfolio Website',
     description: 'Personal portfolio featuring a classical, jekyll inspired design built with Vue and Vite showcasing projects and blog.',
     tech: ['Vue', 'Vite', 'CSS'],
@@ -8,7 +24,7 @@ const projects = [
     image: '',
   },
   {
-    id: 'p2',
+    id: 'p4',
     title: 'CMS Integration',
     description: 'CMS-driven marketing site using headless CMS and static generation.',
     tech: ['PHP', 'Laravel', 'API'],
